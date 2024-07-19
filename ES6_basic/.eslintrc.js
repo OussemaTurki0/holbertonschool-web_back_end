0d6a4749b354dda25165c661891c317b95cbb87e
oussematurki0 <oussematurki0@gmail.com>
2024-07-19 12:35:44 +0200

    commit

--- ES6_basic/.eslintrc.js
+++ ES6_basic/.eslintrc.js
@@ -1,28 +1,35 @@
 module.exports = {
-    env: {
-      browser: true,
-      es6: true,
-      node: true,
-    },
-    extends: [
-      'eslint:recommended',
-      'plugin:react/recommended',
-      'plugin:@typescript-eslint/recommended',
+  env: {
+    browser: false,
+    es6: true,
+    jest: true,
+  },
+  extends: [
+    'airbnb-base',
+    'plugin:jest/all',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['jest'],
+  rules: {
+    'no-console': 'off',
+    'no-shadow': 'off',
+    'no-restricted-syntax': [
+      'error',
+      'LabeledStatement',
+      'WithStatement',
     ],
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-      ecmaFeatures: {
-        jsx: true,
-      },
-      ecmaVersion: 2020,
-      sourceType: 'module',
-    },
-    plugins: [
-      'react',
-      '@typescript-eslint',
-    ],
-    rules: {
-      'react/react-in-jsx-scope': 'off',
-    },
-  };
-  
+  },
+  overrides:[
+    {
+      files: ['*.js'],
+      excludedFiles: 'babel.config.js',
+    }
+  ]
+};
