76a670dae7c9e84f32659fe36d47ff5f198e4fd4
oussematurki0 <oussematurki0@gmail.com>
2024-07-24 16:50:22 +0200

    commit

--- ES6_promise/9-try.js
+++ ES6_promise/9-try.js
@@ -1,15 +1,14 @@
 function guardrail(mathFunction) {
-    const queue = [];
-    try {
-      const result = mathFunction();
-      queue.push(result);
-    } catch (error) {
-      queue.push(`Error: ${error.message}`);
-    } finally {
-      queue.push('Guardrail was processed');
-    }
-    return queue;
+  const queue = [];
+  try {
+    const result = mathFunction();
+    queue.push(result);
+  } catch (error) {
+    queue.push(`Error: ${error.message}`);
+  } finally {
+    queue.push('Guardrail was processed');
   }
-  
-  export default guardrail;
-  
+  return queue;
+}
+
+export default guardrail;
