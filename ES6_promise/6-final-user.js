69e1c72972c4d6ef2274fe33e2a97219aac24cf4
oussematurki0 <oussematurki0@gmail.com>
2024-07-24 16:52:03 +0200

    commit

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,15 +1,15 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-function handleProfileSignup(firstName, lastName, fileName) {
-  const signUpPromise = signUpUser(firstName, lastName);
-  const uploadPromise = uploadPhoto(fileName);
-
-  return Promise.allSettled([signUpPromise, uploadPromise])
-    .then((results) => results.map((result) => ({
-      status: result.status,
-      value: result.status === 'fulfilled' ? result.value : result.reason,
-    })));
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const user = await signUpUser(firstName, lastName);
+  let photo = null;
+  try {
+    photo = await uploadPhoto(fileName);
+  } catch (err) {
+    photo = `${err.name}: ${err.message}`;
+  }
+  const prom1 = { status: 'fulfilled', value: user };
+  const prom2 = { status: 'rejected', value: photo };
+  return [prom1, prom2];
 }
-
-export default handleProfileSignup;
