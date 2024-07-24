ecca64eadcd96161d78ee3f0bee945f727da6c4b
oussematurki0 <oussematurki0@gmail.com>
2024-07-24 15:35:00 +0200

    commit

---
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,14 @@
+export default function updateUniqueItems(groceryMap) {
+    // Vérifie si l'argument passé est une carte
+    if (!(groceryMap instanceof Map)) {
+      throw new Error('Cannot process'); // Impossible de traiter
+    }
+  
+    // Parcourt chaque entrée de la carte
+    groceryMap.forEach((quantity, item) => {
+      // Si la quantité est de 1, met à jour la quantité à 100
+      if (quantity === 1) { groceryMap.set(item, 100); }
+    });
+  
+    return groceryMap; // Retourne la carte mise à jour
+  }
